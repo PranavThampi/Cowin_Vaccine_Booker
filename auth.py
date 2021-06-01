a585cb83d148d96e597fa06ea4ffa2f8996e76a4
Thampi <thampipranav98@gmail.com>
2021-06-01 22:58:11 +0530

    added main.py and auth.py

---
+++ auth.py
@@ -0,0 +1,14 @@
+from utils import Websites
+import requests
+
+
+class Authentication:
+
+    def __init__(self):
+        pass
+
+    def generate_otp(self):
+        pass
+
+    def validate_otp(self):
+        pass
---
+++ main.py
@@ -0,0 +1,13 @@
+from api_calls import ApiCalls
+
+
+class Booking:
+
+    def __init__(self):
+        pass
+
+    def get_beneficiaries(self):
+        pass
+
+    def get_appointment(self):
+        pass
